7dae96745647f7e030bd397ef4862c352fbe82ab
Daniel Parnell <me@danielparnell.com>
2024-01-04 02:55:13 +0000

    use create + start rather than run

--- add_db2.sh
+++ add_db2.sh
@@ -6,6 +6,6 @@ PORT=$2
 DATA_DIR=$HOME/db2/$VERSION
 mkdir -p $DATA_DIR
 
-docker run --restart=always --name db2-$VERSION --privileged=true -p $PORT:50000 -e LICENSE=accept -e DB2INST1_PASSWORD=password -e DBNAME=db2 -v $DATA_DIR:/database ibmcom/db2:$VERSION
-
+docker create --restart=always --name db2-$VERSION --privileged=true -p $PORT:50000 -e LICENSE=accept -e DB2INST1_PASSWORD=password -e DBNAME=db2 -v $DATA_DIR:/database ibmcom/db2:$VERSION
+docker start db2-$VERSION
 
