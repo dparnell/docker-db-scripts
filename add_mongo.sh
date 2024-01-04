9d1b90f37cdeecd865fe04ea5b01dbc0afe0a048
Daniel Parnell <me@danielparnell.com>
2024-01-04 02:46:28 +0000

    Added db2 and updated mssql and mongo

--- add_mongo.sh
+++ add_mongo.sh
@@ -6,5 +6,5 @@ PORT=$2
 DATA_DIR=$HOME/mongo/$VERSION
 mkdir -p $DATA_DIR
 
-docker run --name mongo-$VERSION --restart=always --user "$(id -u):$(id -g)" -v $DATA_DIR:/data/db -p $PORT:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=password -d mongo:$VERSION
+docker run --name mongo-$VERSION --restart=always --user "$(id -u):$(id -g)" --security-opt seccomp=unconfined -v $DATA_DIR:/data/db -p $PORT:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=password -d mongo:$VERSION
 
